391a7e6788a972f8e7c3820b85e807240fa04d98
Viktor Smirnov <smirn04@yandex.ru>
2023-11-10 14:34:51 +0300

    Initial Commit

---
+++ jsonAppNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jsonAppNetwork
+//
+//  Created by Viktor Smirnov on 10.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
