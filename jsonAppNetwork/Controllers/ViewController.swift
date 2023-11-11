3a64fdef6c81031f3f8973e109fae27c34a8ab09
Viktor Smirnov <smirn04@yandex.ru>
2023-11-11 23:14:46 +0300

    Хотел вывести рандомное фото, но ещё не получилось.

--- jsonAppNetwork/Controllers/ViewController.swift
+++ jsonAppNetwork/Controllers/ViewController.swift
@@ -8,16 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func buttonAction(_ sender: UIButton) {
-        
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
     }
     
+    @IBAction func buttonAction(_ sender: UIButton) {
+        getUrlPhoto()
+        getPhoto()
+    
+        
+        
+    }
+    private func getUrlPhoto() {
+        URLSession.shared.dataTask(with: DogRandom.init().urlRandom) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let urlPhoto = try jsonDecoder.decode(GetUrlPhoto.self, from: data)
+                let newMessage = String(urlPhoto.message)
+                print(newMessage)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+//            print(GetUrlPhoto.init(message: newMessage, status: String))
+//            guard let urlPhoto = try jsonDecoder.decode(GetUrlPhoto.self, from: data) else {
+//                return
+//            }
+//            urlPhoto.message
+        }.resume()
+    }
+    
+    
+    private func getPhoto() {
+        URLSession.shared.dataTask(with: DogRandom.init().urlDogRandom) { data, response, error in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.imageView.image = image
+                self.activityIndicator.stopAnimating()
+            }
+        }.resume()
+    }
 }
-
--- jsonAppNetwork/Models/Content.swift
+++ jsonAppNetwork/Models/Content.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct Content {
+    let name: String
+    let rotation_period: String
+    let orbital_period: String
+    let diameter: String
+    let climate: String
+    let gravity: String
+    let terrain: String
+    let surface_water: String
+    let population: String
+    let residents: [Any]
+    let films: [Any]
+    let created: String
+    let edited: String
+    let url: String
+    let urlNeed: String = "https://swapi.dev/api/planets/3/?format=json"
+}
+
+struct DogRandom {
+    let urlRandom = URL(string: "https://dog.ceo/api/breeds/image/random")!
+    let urlDogRandom = URL(string: "https://images.dog.ceo/breeds/saluki/n02091831_8847.jpg")!
+}
+
+
+struct GetUrlPhoto: Decodable {
+    let message: String
+    let status: String
+}
