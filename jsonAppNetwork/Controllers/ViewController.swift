d3fe3099b5bbfad6aaf4c82a62879c12c9861c86
Viktor Smirnov <smirn04@yandex.ru>
2023-11-12 17:01:30 +0300

    final поставил.

--- jsonAppNetwork/Controllers/ViewController.swift
+++ jsonAppNetwork/Controllers/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
